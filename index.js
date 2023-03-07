1bed8cfbfe929a939c8be33388bc8b8311e7fdd6
Winston Sebastian Pais <51374812+WinstonPais@users.noreply.github.com>
2023-03-07 15:07:07 +0530

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+var config = require('./config');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send(`Hello World! ${config.NAME}`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
